552a15427e2234388be83823f16b0afaa0e72270
Paweł Kowalski <jordan.kozuch90@gmail.com>
2019-04-15 15:06:29 +0200

    Latest orders page demo

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,14 @@
+import { LINE_CHART_COLORS } from './../../shared/chart.colors';
 import { Component, OnInit } from '@angular/core';
+
+const LINE_CHART_SAMPLE_DATA: any[]=[
+  {data:[21,14,25,24,22,33],label: 'Sentiment Analysis'},
+  {data:[21,34,15,22,12,30],label: 'Image Recognition'},
+  {data:[31,24,21,32,14,25],label: 'Forecasting'}
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan','Feb','Mar','May','Jun']
+
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +18,16 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions:any={
+    responsive:true,
+    maintainAspectRatio:false
+  };
+  lineChartLegend=true;
+  lineChartType='line';
+  lineChartColors=LINE_CHART_COLORS;
 
   ngOnInit() {
   }
--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,6 @@
+import { RouterModule } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import {Order} from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +10,14 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {id:1, customer: {id: 1,name: 'Andaluzja',state:'CO',email: 'mainst@example.com'}, total:240,placed: new Date(217,12,1),fulfilled:new Date(217,12,1)},
+    {id:2, customer: {id: 1,name: 'Andaluzja',state:'CO',email: 'mainst@example.com'}, total:240,placed: new Date(217,12,1),fulfilled:new Date(217,12,1)},
+    {id:3, customer: {id: 1,name: 'Andaluzja',state:'CO',email: 'mainst@example.com'}, total:240,placed: new Date(217,12,1),fulfilled:new Date(217,12,1)},
+    {id:4, customer: {id: 1,name: 'Andaluzja',state:'CO',email: 'mainst@example.com'}, total:240,placed: new Date(217,12,1),fulfilled:new Date(217,12,1)},
+    {id:5, customer: {id: 1,name: 'Andaluzja',state:'CO',email: 'mainst@example.com'}, total:240,placed: new Date(217,12,1),fulfilled:new Date(217,12,1)}
+  ];
 
   ngOnInit() {
   }
